7d0d0eab0247621d48ec7edf8b83843a9efcf27c
John Kieran Palladino <johnpalladino0@gmail.com>
2023-03-23 18:14:44 -0400

    Initial Commit

---
+++ Reflex/ReflexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReflexApp.swift
+//  Reflex
+//
+//  Created by John Kieran Palladino on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ReflexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
